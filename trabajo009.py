cbf0050d934b6e4798e505a7425dee682b51bde8
Emilioloopz <117968357+Emilioloopz@users.noreply.github.com>
2022-11-11 00:01:32 -0600

    Add files via upload

---
+++ trabajo009.py
@@ -0,0 +1,8 @@
+days = int(inpu('enter the number of days'))
+hours= days*24
+minutes= hours *60
+seconds =minutes*60
+print('in', days 'days there are')
+print(hours, 'hours')
+print(minutes, 'minutes')
+print(seconds, 'seconds')
